3507f2e0729c329afef0ca17a87b0e40635dd1bb
Shadab Khatib <shadab.khatib@gmail.com>
2026-01-18 16:26:25 +0530

    config: updated the database config setup

--- api/config.php
+++ api/config.php
@@ -1,8 +1,8 @@
 <?php
 $pdo = new PDO(
-    "mysql:host=127.0.0.1;port=3308;dbname=orthodb;charset=utf8mb4",
-    "root",
-    "root8039",
+    "mysql:host=127.0.0.1;port=3306;dbname=orthoui_db;charset=utf8mb4",
+    "orthoui_user",
+    "OrthoPass123!",
     [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
